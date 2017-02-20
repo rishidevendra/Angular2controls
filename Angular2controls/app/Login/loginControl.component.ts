3774c0a11cc219b669a2d03c7f48fe4b6b65e230
devendrar <rishidevendra87@gmail.com>
2017-02-20 17:14:54 -0500

    Made changes in form control

--- Angular2controls/app/Login/loginControl.component.ts
+++ Angular2controls/app/Login/loginControl.component.ts
@@ -3,22 +3,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
     moduleId: module.id,
     selector: 'my-form',
-    templateUrl : 'loginControl.component.html'
+    templateUrl: 'loginControl.component.html'
 })
 export class StudentFormComponent {
-    form = new FormGroup({
-        username: new FormControl('Nancy', Validators.required),
-        password: new FormControl('', Validators.required),
-    });
-    get username(): any
-    {
-        return this.form.get('username');
+    public user: any;
+
+    ngOnInit() {
+        this.user = {
+            name: '',
+            address: {
+                street: '',
+                postcode: '8000'
+            }
+        };
     }
-    get password(): any {
-        return this.form.get('password');
+
+    save(loginForm: any) {
+        // console.log(model, isValid);
+        console.log("Save submitted");
     }
+    model: any = {};
+    //get username(): any
+    //{
+    //    return this.form.get('username');
+    //}
+    //get password(): any {
+    //    return this.form.get('password');
+    //}
     onSubmit(): void {
-        console.log(this.form.value);  // {first: 'Nancy', last: 'Drew'}
+        console.log("Form submitted");  // {first: 'Nancy', last: 'Drew'}
     }
-   
+
+    login(): void {
+        console.log("Form submitted on click");  // {first: 'Nancy', last: 'Drew'}
+    }
+
 }
